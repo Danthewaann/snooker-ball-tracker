af78350ec22dd358c74f750b48283d0319092e9d
dcrblack <danielcrblack@gmail.com>
2021-04-06 13:09:28 +0100

    Removing redundant imports from __main__.py

--- src/snooker_ball_tracker/__main__.py
+++ src/snooker_ball_tracker/__main__.py
@@ -1,8 +1,6 @@
 import argparse
 import sys
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import *
-from PyQt5.QtCore import *
+from PyQt5.QtWidgets import QApplication
 from snooker_ball_tracker.views import MainView
 
 if __name__ == "__main__":
