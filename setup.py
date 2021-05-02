5aae705ea2368aa8b9baf4b43c4be500504d9dc6
dcrblack <danielcrblack@gmail.com>
2021-05-02 01:49:26 +0100

    setup.py and pyinstaller changes

--- setup.py
+++ setup.py
@@ -1,12 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name='snooker_ball_tracker',
-    version='0.1.dev0',
-    description='Demo app that tracks balls on a Snooker table',
-    author='Daniel Black',
-    author_email='danielcrblack@gmail.com',
-    package_dir={'': 'src'},
-    packages=find_packages('src'),
-    install_requires=['numpy', 'imutils', 'opencv-python', 'Pillow', 'pyqt5']
-)
+
+setup()
