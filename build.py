292b307e48914ebc42227e371ca0114ea944c8cd
Daniel Black <danielcrblack@gmail.com>
2021-09-26 13:39:53 +0100

    Added poetry for package management (#7)
    * Switched over to poetry package management
    
    * Update dependencies
    
    * More changes
    
    * README change

--- build.py
+++ build.py
@@ -1,42 +1,50 @@
 import os
 import pathlib
 import shutil
-import sys
 
 import PyInstaller.__main__
 import pyinstaller_versionfile
-from setuptools.config import read_configuration
-
-from src.snooker_ball_tracker import __version__
+import tomlkit
 
 BASE_DIR = pathlib.Path(__file__).parent
-CONF = read_configuration(BASE_DIR / "setup.cfg")
 
 
 os.environ["PYTHONOPTIMIZE"] = "1"
 
 
+def _get_project_meta():
+    with open('./pyproject.toml') as pyproject:
+        file_contents = pyproject.read()
+
+    return tomlkit.parse(file_contents)['tool']['poetry']
+
+
+pkg_meta = _get_project_meta()
+project = str(pkg_meta['name'])
+version = str(pkg_meta['version'])
+
+
 pyinstaller_versionfile.create_versionfile(
     output_file=str(BASE_DIR / "version_file.txt"),
-    version=__version__+".0",
+    version=version + ".0",
     file_description="Snooker Ball Tracker",
     internal_name="Snooker Ball Tracker",
-    original_filename=CONF["metadata"]["name"]+".exe",
+    original_filename=project + ".exe",
     product_name="Snooker Ball Tracker"
 )
 
 
 PyInstaller.__main__.run([
-    str(BASE_DIR / "src" / CONF["metadata"]["name"] / "gui.py"),
+    str(BASE_DIR / "src" / project / "gui.py"),
     "--onefile",
     "--windowed",
     "-y",
-    "--add-data", str(BASE_DIR / "src" / CONF["metadata"]["name"] / "resources" / "icon.ico") + ":icon.ico",
-    "-i", str(BASE_DIR / "src" / CONF["metadata"]["name"] / "resources" / "icon.ico"),
-    "-n", CONF["metadata"]["name"]+".exe",
+    "--add-data", str(BASE_DIR / "src" / project / "resources" / "icon.ico") + ":icon.ico",
+    "-i", str(BASE_DIR / "src" / project / "resources" / "icon.ico"),
+    "-n", project + ".exe",
     "--clean",
     "--version-file", str(BASE_DIR / "version_file.txt")
 ])
 
-shutil.copy(str(BASE_DIR / "src" / "snooker_ball_tracker" / "resources" / "icon.ico"), str(BASE_DIR / "dist"))
-shutil.copy(str(BASE_DIR / "src" / "snooker_ball_tracker" / "resources" / "default_settings.json"), str(BASE_DIR / "dist"))
+shutil.copy(str(BASE_DIR / "src" / project / "resources" / "icon.ico"), str(BASE_DIR / "dist"))
+shutil.copy(str(BASE_DIR / "src" / project / "resources" / "default_settings.json"), str(BASE_DIR / "dist"))
